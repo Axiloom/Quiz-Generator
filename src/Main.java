964b2dfd8cf33fb20cb80bfb0dcc2b3b1b94f7b9
Joe <37149816+Jalessner@users.noreply.github.com>
2019-04-22 16:15:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+package application;
+	
+import javax.swing.JFrame;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -6,9 +9,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 
@@ -16,7 +21,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
     try {  
-      Scene scene = new Scene(getMainMenu(), 450, 450);
+      Scene scene = new Scene(getSaveMenu(), 500, 500);
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
       primaryStage.setScene(scene);
       primaryStage.setTitle("Quiz Generator");
@@ -29,19 +34,17 @@ public class Main extends Application {
   private BorderPane getMainMenu() {
 
     Label label = new Label("Main Menu");
-    Label numQuestions = new Label("5 Questions available");
+    Label numQuestions = new Label("X Questions available");
     
     ObservableList<String> topics =
         FXCollections.observableArrayList("Linux", "ADTs", "Search Trees");
     ComboBox<String> topicBox = new ComboBox<>(topics);
-    topics.add("Example");
     
     BorderPane root = new BorderPane();
     
-    Button load = new Button("Load Questions");
-    Button add = new Button(" Add Questions");
-    Button save = new Button("Save Questions");
-//    Button setTopic = new Button("     Set Topic     ");
+    Button load = new Button("  Load Questions ");
+    Button add = new Button("   Add Questions ");
+    Button save = new Button("  Save Questions ");
     Button next = new Button("START");
     
     Image one = new Image("100x100blank.png");
@@ -60,8 +63,49 @@ public class Main extends Application {
     
     return root;
   }
-	
+
+  private BorderPane getSaveMenu() {
+    Label label = new Label("Save Menu");
+    Label numQuestions = new Label("X Questions available");
+    
+    BorderPane root = new BorderPane();
+    
+    Label jsonLabel = new Label("Save as JSON:");
+    TextField jsonName = new TextField("Enter JSON File here");
+    
+    Button next = new Button("SAVE");
+    Button back = new Button("BACK");
+    
+    Image two = new Image("150x150blank.png");
+    Image three = new Image("100x300blank.png");
+    ImageView img2 = new ImageView(two);
+    ImageView img3 = new ImageView(three);
+    ImageView img4 = new ImageView(three);
+    
+    VBox leftVBox = new VBox(label, img4, back);
+    VBox rightVBox = new VBox(numQuestions, img3, next);
+    VBox centerVBox = new VBox(img2, jsonLabel, jsonName);
+    root.setCenter(centerVBox);
+    root.setLeft(leftVBox);
+    root.setRight(rightVBox);
+    
+    return root;
+  }
+
+  //TODO
+  private BorderPane getLoadMenu() {
+    BorderPane root = new BorderPane();
+    return root;
+  }
+
+  //TODO
+  private BorderPane getAddMenu() {
+    BorderPane root = new BorderPane();
+    return root;
+  }
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
